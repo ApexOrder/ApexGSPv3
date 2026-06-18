53f880cb6b3d1b45bb75c10a23c800255607a118
Apex <32912837+ApexOrder@users.noreply.github.com>
2026-06-18 11:56:43 +0100

    Register server config update job

--- daemon/src/jobs/index.ts
+++ daemon/src/jobs/index.ts
@@ -2,6 +2,7 @@ import { testPing } from './testPing.js'
 import { detectTools } from './detectTools.js'
 import { installSteamcmd } from './installSteamcmd.js'
 import { createServer } from './createServer.js'
+import { updateServerConfig } from './config.js'
 import { refreshServerStatus, restartServer, startServer, stopServer } from './manageServer.js'
 import { getServerLogs } from './serverConsole.js'
 
@@ -19,6 +20,8 @@ export async function runJob(type: string, payload: unknown, ctx?: JobContext) {
       return installSteamcmd(payload, ctx)
     case 'create_server':
       return createServer(payload, ctx)
+    case 'update_server_config':
+      return updateServerConfig(payload, ctx)
     case 'start_server':
       return startServer(payload, ctx)
     case 'stop_server':
